d424429a1e83c8e5ed87c1d0b627955d0f17f70a
lwjnicole <2570908152@qq.com>
2017-12-27 11:16:40 +0800

    新增SiteServiceImpl实现类，并实现站点管理新增站点功能

---
+++ src/com/lwjnicole/service/impl/SiteServiceImpl.java
@@ -0,0 +1,26 @@
+package com.lwjnicole.service.impl;
+
+import java.sql.SQLException;
+
+import com.lwjnicole.Dao.SiteDao;
+import com.lwjnicole.domain.Site;
+import com.lwjnicole.service.SiteService;
+import com.lwjnicole.utils.BeanFactory;
+
+/**
+ * 站点模块的service接口的实现类
+ * @author user
+ *
+ */
+public class SiteServiceImpl implements SiteService {
+
+	/**
+	 * 新增功能service
+	 */
+	@Override
+	public void addSite(Site site) throws SQLException{
+		SiteDao siteDao = (SiteDao) BeanFactory.getBean("siteDao");
+		siteDao.addSite(site);
+	}
+
+}
