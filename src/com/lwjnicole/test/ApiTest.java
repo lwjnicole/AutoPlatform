7d0df4aceae75ad027b8a2aa899f988ffd2afdd6
lwjnicole <2570908152@qq.com>
2018-01-14 18:42:29 +0800

    修改测试用例的执行方法

--- src/com/lwjnicole/test/ApiTest.java
+++ src/com/lwjnicole/test/ApiTest.java
@@ -2,8 +2,11 @@ package com.lwjnicole.test;
 
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
 import com.lwjnicole.vo.CaseVo;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static org.hamcrest.CoreMatchers.*;  
 import static com.jayway.restassured.RestAssured.*;
@@ -23,13 +26,22 @@ public class ApiTest {
 		String cmethod = caseVo.getCmethod();
 		String curl = caseVo.getCurl();
 		String cparam = caseVo.getCparam();
+		String cparamtype = caseVo.getCparamtype();
+		String cheader = caseVo.getCheader();
+		String cresult = caseVo.getCresult();
+		
 		if(cmethod !=null){
 			if("GET".equals(cmethod)){
-				Response response = RestAssured.get(curl+cparam);
-				System.out.println(response.asString());
-			}else
-				if ("POST".equalsIgnoreCase(cmethod)){
-					
+				if("FORM".equals(cparamtype)){
+					Response response = RestAssured.get(curl+cparam);
+					System.out.println(response.asString());
+					JsonPath jsonPath = new JsonPath(response.asString());
+					String[] result = cresult.split("=");
+					System.out.println(result[0] + "   " + result[1]);
+					String expectResult = jsonPath.getString(result[0]);
+					Assert.assertEquals(expectResult, "111");
+				}
+			}else if("POST".equalsIgnoreCase(cmethod)){
 						given().
 						header("Content-Type", "application/json").
 						body(cparam).
